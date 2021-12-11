21bde89a33508c50ec5cfcdb87510ad9f68d4c66
Roger Holmgren <roger.holmgren@hrmsoftware.se>
2021-12-11 15:36:23 +0100

    Simple post test

--- src/test/java/com/test/todo/TodoApplicationTests.java
+++ src/test/java/com/test/todo/TodoApplicationTests.java
@@ -1,27 +1,42 @@
 package com.test.todo;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
+
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @ExtendWith(SpringExtension.class)
 @SpringBootTest
 @AutoConfigureMockMvc
 class TodoApplicationTests {
 
-	@Autowired
-	private MockMvc mockMvc;
+    @Autowired
+    private MockMvc mockMvc;
 
-	@Test
-	void contextLoads() throws Exception {
-		mockMvc.perform(get("/todos"))
-				.andExpect(status().isOk());
-	}
+    @Autowired
+    private ObjectMapper objectMapper;
 
+    @Test
+    void contextLoads() throws Exception {
+        mockMvc.perform(get("/todos"))
+                .andExpect(status().isOk());
+    }
+
+    @Test
+    void postTest() throws Exception {
+        mockMvc.perform(post("/todos")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(new Todo("hej", false))))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.title").value("hej"));
+    }
 }
