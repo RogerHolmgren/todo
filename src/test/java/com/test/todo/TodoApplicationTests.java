47d3b8f8048e3ec35fde83e737316d30be63d758
Roger Holmgren <roger.holmgren@hrmsoftware.se>
2021-12-11 15:19:46 +0100

    One working test

--- src/test/java/com/test/todo/TodoApplicationTests.java
+++ src/test/java/com/test/todo/TodoApplicationTests.java
@@ -1,13 +1,27 @@
 package com.test.todo;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.web.servlet.MockMvc;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
+@ExtendWith(SpringExtension.class)
 @SpringBootTest
+@AutoConfigureMockMvc
 class TodoApplicationTests {
 
+	@Autowired
+	private MockMvc mockMvc;
+
 	@Test
-	void contextLoads() {
+	void contextLoads() throws Exception {
+		mockMvc.perform(get("/todos"))
+				.andExpect(status().isOk());
 	}
 
 }
